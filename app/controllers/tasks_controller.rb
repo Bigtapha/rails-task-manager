7760c82df7f3c36bac47631713702f35a3edadcc
moustapha cisse <ctapha13@gmail.com>
2019-11-05 17:21:33 +0100

    rails-task-manager-done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -14,11 +15,33 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
+
+    redirect_to '/tasks'
   end
 
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    # @task.update_column(completed: true) if params[:completed] == '1'
+
+    redirect_to '/tasks', notice: 'task have been updated'
+  end
+
+  def destroy
+    @task.destroy
+
+    redirect_to '/tasks', notice: "#{@task.title} have been successfully destroyed"
+  end
+  
   private
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # GET /TASKS
-  get 'tasks', to: 'tasks#index', as: :restaurants
+  get 'tasks', to: 'tasks#index', as: :tasks
 
   # GET /NEW and POST/NEW
-  get 'tasks/new', to: 'tasks#new'
-  post 'tasks', to: 'tassks#create'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
 
   # GET /TASK/:id
   get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # GET /NEW and POST/NEW
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # DELETE /restaurants/:id
+  delete 'tasks/:id', to: 'tasks#destroy', as: :destroy
 end
